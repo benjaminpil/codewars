e31638add85055380fcf4e2556e8f38d49edb89a
Benjamin Pil <benjaminpil@gmail.com>
2022-04-03 20:02:20 +0200

    added another solved 8kyu

--- main.js
+++ main.js
@@ -12,3 +12,9 @@ function squareDigits(num){
   function greet(){
     return "hello world!"
   }
+
+//Complete the solution so that it reverses all of the words within the string passed in.
+//Example: "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+  function reverseWords(str){
+    return str.split(" ").reverse().join(" ");
+  }
