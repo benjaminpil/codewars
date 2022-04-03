c20e5045cd0e31fb5a1e91c359e8397356ced7ee
Benjamin Pil <benjaminpil@gmail.com>
2022-04-03 19:39:39 +0200

    cleaned up the first file

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 // Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
-
 // For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
-
 // Note: The function accepts an integer and returns an integer
 
 function squareDigits(num){
@@ -9,5 +7,3 @@ function squareDigits(num){
     let arrSquare = arrNum.map((x) => Number(x)**2)
     return Number(arrSquare.join(''))
   }
-
-  //test
