f8d171746c59464ea4c3325d0c4ff5253ab3acc8
Benjamin Pil <benjamin.pil@knokke-heist.be>
2022-04-04 12:51:19 +0200

    added another 8kyu

--- main.js
+++ main.js
@@ -27,3 +27,13 @@ function squareDigits(num){
     let arrNum = String(n).split("")
     return Number(arrNum.sort((a,b) => b-a).join(""))
   }
+
+//Build a function that returns an array of integers from n to 1 where n>0.
+//Example : n=5 --> [5,4,3,2,1]
+const reverseSeq = n => {
+  let arrNums = []
+  for(let i = 1; i<=n; i++){
+    arrNums.push(i)
+  }
+  return arrNums.reverse();
+};
