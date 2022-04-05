4b12fb9db7e28273bd6e94afcfef883011a9bbb3
Benjamin Pil <benjaminpil91@gmail.com>
2022-04-05 21:07:55 +0200

    Code Challenge and solution 2022-04-05

--- CC-2022-04-05/app.js
+++ CC-2022-04-05/app.js
@@ -2,3 +2,8 @@
 function booleanToString(b){
     return b ? "true" : "false"
 }
+
+// Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor. First argument is an array of numbers and the second is the divisor.
+function divisibleBy(numbers, divisor) {
+    return numbers.filter(n => n % divisor === 0)
+  }
