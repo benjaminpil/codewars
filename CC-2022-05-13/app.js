905795df83d8c969bdca548e79c2443822ca1e8e
Benjamin Pil <benjaminpil91@gmail.com>
2022-05-13 16:42:56 +0200

    compleyed a 7kyu codewars

--- CC-2022-05-13/app.js
+++ CC-2022-05-13/app.js
@@ -21,3 +21,43 @@ function points(games) {
     }
     return total
   }
+
+  // Rock Paper Scissors
+  // Let's play! You have to return which player won! In case of a draw return Draw!.
+  
+  // Examples:
+  
+  // rps('scissors','paper') // Player 1 won!
+  // rps('scissors','rock') // Player 2 won!
+  // rps('paper','paper') // Draw!
+
+  const rps = (p1, p2) => {
+    if(p1 === 'scissors' && p2 === 'paper' || p1 === 'paper' && p2 === 'rock' || p1 === 'rock' && p2 === 'scissors'){
+      return 'Player 1 won!'
+    } else if (p1 === 'scissors' && p2 === 'rock' || p1 === 'paper' && p2 === 'scissors' || p1 === 'rock' && p2 === 'paper'){
+      return 'Player 2 won!'
+    } else{
+      return 'Draw!'
+    }
+  };
+
+  // Your task is to create the functionisDivideBy (or is_divide_by) to check if an integer number is divisible by both integers a and b.
+
+  function isDivideBy(number, a, b) {
+    return number % a === 0 && number % b === 0
+  }
+
+  // Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+  // Examples input/output:
+  
+  // XO("ooxx") => true
+  // XO("xooxx") => false
+  // XO("ooxXm") => true
+  // XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+  // XO("zzoo") => false
+  
+  const XO = str => {
+    str = str.toLowerCase().split('');
+    return str.filter(x => x === 'x').length === str.filter(x => x === 'o').length;
+  }
